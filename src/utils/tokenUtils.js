34636df5252aaaf9f7a55163f8718db3b8680551
CarlosEbb <167257281+cebolivarb@users.noreply.github.com>
2024-08-02 17:38:22 -0400

    tiempo de inactividad

--- src/utils/tokenUtils.js
+++ src/utils/tokenUtils.js
@@ -24,5 +24,5 @@ export async function generateResetToken(userId) {
 }
 
 export async function generateAuthToken(user) {
-    return jwt.sign({ id: user.id, rol_id: user.rol_id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+    return jwt.sign({ id: user.id, rol_id: user.rol_id }, process.env.JWT_SECRET, { expiresIn: '10m' });
 }
